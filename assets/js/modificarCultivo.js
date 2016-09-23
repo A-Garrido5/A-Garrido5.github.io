85dbd57a878e7bf008d9854e198d12366dcea435
A-Garrido5 <adrian@manglar.cl>
2016-09-23 12:10:13 -0300

    Moviendo paneles de acuerdo a cada boton

--- assets/js/modificarCultivo.js
+++ assets/js/modificarCultivo.js
@@ -19,29 +19,60 @@ $('#editard').click(function () {
     }, 800);
 }); */
 
-// diferencia 362
+// diferencia 360
 
-// 6 paneles 2169
-// 5 paneles 1807
+// 6 paneles 2159		
+// 5 paneles 1799
 
-// 4 paneles 1445	
-// 3 paneles 1082
+// 4 paneles 1438	
+// 3 paneles 1077
 
-// 2 paneles 720
+// 2 paneles 719
 
 
-function moveScroll2() {
-	var scroll = $('#as').scrollLeft();
-	
+function moveScroll2(pos) {
 
 	var elem = $("#as");
+
+	var scroll = $('#as').scrollLeft();
 	var anchoPantalla = elem[0].scrollWidth
-	var maxScrollTop = elem[0].scrollWidth - elem.outerWidth();
+
+	switch (pos){
+		case 1:
+
+			$('#as').scrollLeft($('#panel1')[0].offsetLeft-(anchoPantalla/100));
+			break;
+
+		case 2:
+
+			$('#as').scrollLeft($('#panel2')[0].offsetLeft-(anchoPantalla/100));
+			break;
+
+		case 3:
+
+			$('#as').scrollLeft($('#panel3')[0].offsetLeft-(anchoPantalla/100));
+			break;
+	}
+
+
+	
+	
+
+	//console.log(scroll)
+	
+	//
+	//var anchoPantalla = elem[0].scrollWidth
+	//var maxScrollTop = elem[0].scrollWidth - elem.outerWidth();
     
+	//$('#as').scrollLeft()
+	//$('#as').scrollLeft(scroll+50.8*(elem.outerWidth()/100))
 
-    $('#as').scrollLeft(scroll+52*(elem.outerWidth()/100))
+	
+    //$('#as').scrollLeft(scroll+50.069541029*(elem.outerWidth()/100))
 
-    console.log()
+    //console.log(anchoPantalla)
+
+    //console.log(Math.round(anchoPantalla/361.5))
 }
 
     
