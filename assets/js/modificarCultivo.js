588949065f3fe647d8f124100f48c6e3ddd01074
A-Garrido5 <adrian@manglar.cl>
2016-09-26 11:22:47 -0300

    Corregida vista de notificaciones

--- assets/js/modificarCultivo.js
+++ assets/js/modificarCultivo.js
@@ -2,14 +2,11 @@ $(document).on("click", "#editar", function(e) {
 
 	var $elem = $('#editar');
 
-    // we use a pseudo object for the animation
-    // (starts from `0` to `angle`), you can name it as you want
+    
     $({deg: 0}).animate({deg: 45}, {
         duration: 50,
         step: function(now) {
-            // in the step-callback (that is fired each step of the animation),
-            // you can use the `now` paramter which contains the current
-            // animation-position (`0` up to `angle`)
+            
             $elem.css({
                 transform: 'rotate(' + now + 'deg)'
             });
@@ -24,14 +21,10 @@ $(document).on("click", "#fade", function(e) {
 
 	var $elem = $('#editar');
 
-    // we use a pseudo object for the animation
-    // (starts from `0` to `angle`), you can name it as you want
     $({deg: 0}).animate({deg: -90}, {
         duration: 50,
         step: function(now) {
-            // in the step-callback (that is fired each step of the animation),
-            // you can use the `now` paramter which contains the current
-            // animation-position (`0` up to `angle`)
+            
             $elem.css({
                 transform: 'rotate(' + now + 'deg)'
             });
@@ -42,14 +35,6 @@ $(document).on("click", "#fade", function(e) {
 	document.getElementById('light').style.display='none';
 });
 
-/*
-$('#editard').click(function () {
-    var leftPos = $('#as').scrollLeft();
-    console.log(leftPos);    
-    $("as").animate({
-        scrollLeft: leftPos + 700
-    }, 800);
-}); */
 
 
 var lastScrollLeft = 0;
@@ -60,11 +45,7 @@ $('#as').scroll(function() {
 
 	var scroll = $('#as').scrollLeft();
 
-	console.log(scroll)
-
 	var anchoPantalla = elem[0].scrollWidth
-
-	console.log(anchoPantalla)
 
 	if(scroll < anchoPantalla/4){
 		$('#1').css("background","white");
@@ -88,15 +69,6 @@ $('#as').scroll(function() {
 	}    
 });
 
-// diferencia 360
-
-// 6 paneles 2159		
-// 5 paneles 1799
-
-// 4 paneles 1438	
-// 3 paneles 1077
-
-// 2 paneles 719
 
 
 function moveScroll2(pos) {
@@ -140,21 +112,6 @@ function moveScroll2(pos) {
 	
 	
 
-	//console.log(scroll)
-	
-	//
-	//var anchoPantalla = elem[0].scrollWidth
-	//var maxScrollTop = elem[0].scrollWidth - elem.outerWidth();
-    
-	//$('#as').scrollLeft()
-	//$('#as').scrollLeft(scroll+50.8*(elem.outerWidth()/100))
-
-	
-    //$('#as').scrollLeft(scroll+50.069541029*(elem.outerWidth()/100))
-
-    //console.log(anchoPantalla)
-
-    //console.log(Math.round(anchoPantalla/361.5))
 }
 
     
