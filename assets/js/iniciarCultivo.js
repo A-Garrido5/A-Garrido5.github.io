786f1cd26b9ca7bd7766f5f936fc9adcc93a859e
A-Garrido5 <adrian@manglar.cl>
2016-10-03 11:27:39 -0300

    Implementando correctamente los alert

--- assets/js/iniciarCultivo.js
+++ assets/js/iniciarCultivo.js
@@ -3,12 +3,18 @@ $('#select_program').on('change', function() {
 });
 
 
+$(document).on("click", "#boton", function(e) {
+
  
-document.getElementById('fade').style.display='block';
+	document.getElementById('fade').style.display='block';	    
+	document.getElementById('light').style.display='block';
 
-	    
-document.getElementById('light').style.display='block';
+});
 
+$(document).on("click", "#fade", function(e) {
 
+	document.getElementById('fade').style.display='none';	    
+	document.getElementById('light').style.display='none';
 
+});
 
